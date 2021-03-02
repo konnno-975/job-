d0e6f815558eb79ae853c5131574312824ba41cd
Yuki Konno <sbaningu@gmail.com>
2021-03-02 22:10:21 +0900

    2021-3-2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   post 'job/create' => 'job#create'
   post 'job/:id/update' => 'job#update'
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   get '/' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
